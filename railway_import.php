d1a502a9adb19c3895e411808c945c00e4bc03cb
facuvar <tu-email@ejemplo.com>
2025-08-15 09:55:05 -0300

    Script verificación DB + archivos sincronización

--- railway_import.php
+++ railway_import.php
@@ -5,6 +5,6 @@ require_once 'config/database.php';
 $config = require 'config/database.php';
 $pdo = new PDO("mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4", $config['username'], $config['password']);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = file_get_contents('railway_sync_2025-08-15_14-41-18.sql');
+$sql = file_get_contents('railway_sync_2025-08-15_14-42-36.sql');
 $pdo->exec($sql);
 echo 'Importación completada!';
